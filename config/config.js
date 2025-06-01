9f3f8530d60a283109f0e033b68d428f0a083cd0
Quoc Huy <huyhqse173627@fppt.edu.vn>
2025-06-02 01:38:55 +0700

    .

--- config/config.js
+++ config/config.js
@@ -4,10 +4,10 @@ dotenv.config();
 
 export default {
   development: {
-    username: process.env.DB_USER, // root
-    password: process.env.DB_PASS, // (rỗng)
-    database: process.env.DB_NAME, // sneakershoe
-    host: process.env.DB_HOST, // localhost
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
     port: process.env.DB_PORT || 3306,
     dialect: 'mysql',
   },
--- config/connectDB.js
+++ config/connectDB.js
@@ -9,7 +9,6 @@ export const sequelize = new Sequelize(
     {
         host: process.env.DB_HOST,
         dialect: 'mysql',      // ⚠️ phải khai báo rõ dialect
-        logging: false,
         port: process.env.DB_PORT || 3306,      // (tuỳ chọn) để tắt log SQL query
     }
 );
